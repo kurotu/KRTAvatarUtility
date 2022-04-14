588562d4c19826d040bbefbfbc76f8192fb1a251
kurotu <kurotu@gmail.com>
2022-04-14 21:01:47 +0900

    Add export

---
+++ Editor/KRTAvatarUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace KRT.KRTAvatarUtility
+{
+    static class KRTAvatarUtility
+    {
+        private const string EditorGUID = "9287031fcccc8c646b13d734fc9f0cdc";
+        internal static string AssetRoot => Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(EditorGUID));
+
+        public static void ExportUnityPackage(string filename)
+        {
+            AssetDatabase.ExportPackage(AssetRoot, filename, ExportPackageOptions.Recurse);
+        }
+
+        private static void Export()
+        {
+            ExportUnityPackage("KRTAvatarUtility.unitypackage");
+        }
+    }
+}
