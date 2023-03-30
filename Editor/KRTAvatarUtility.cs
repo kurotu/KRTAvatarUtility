841257d53ac9652ce0104ac68f02eb82dcc0ae6e
kurotu <kurotu@gmail.com>
2023-03-31 00:22:22 +0900

    Add asmdef

--- Editor/KRTAvatarUtility.cs
+++ Editor/KRTAvatarUtility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace KRT.KRTAvatarUtility
 {
-    static class KRTAvatarUtility
+    public static class KRTAvatarUtility
     {
         private const string EditorGUID = "9287031fcccc8c646b13d734fc9f0cdc";
         internal static string AssetRoot => Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(EditorGUID));
